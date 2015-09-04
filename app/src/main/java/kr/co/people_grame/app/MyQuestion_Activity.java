0ff65a8b1671f30e4f007511e067a9f01c58f599
saga1654 <saga1654@naver.com>
2015-09-04 17:46:34 +0900

    myQuestion Activity 수정

--- app/src/main/java/kr/co/people_grame/app/MyQuestion_Activity.java
+++ app/src/main/java/kr/co/people_grame/app/MyQuestion_Activity.java
@@ -2,36 +2,50 @@ package kr.co.people_grame.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MyQuestion_Activity extends AppCompatActivity {
+    private TextView tv_my_question_activity_title, tv_my_question_activity_page;
+    private static int questionNum = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_question_);
+
+        tv_my_question_activity_title = (TextView) findViewById(R.id.tv_my_question_activity_title);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my_question_, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void btn_prev(View v) {
+        if(questionNum == 1) {
+            Toast.makeText(this, "첫페이지", Toast.LENGTH_SHORT).show();
+        } else {
+            getQuestionNum_minus();
+            Log.d("people_gram", String.valueOf(questionNum));
+            //tv_my_question_activity_page.setText(String.valueOf(questionNum) + "/" + "10");
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void btn_next(View v)
+    {
+        if(questionNum == 10) {
+            Toast.makeText(this, "마지막페이지", Toast.LENGTH_SHORT).show();
+        } else {
+            questionNum_plus();
+            Log.d("people_gram", String.valueOf(questionNum));
+            //tv_my_question_activity_page.setText(String.valueOf(questionNum) + "/" + "10");
+        }
+    }
+
+    private void questionNum_plus() {
+        questionNum = questionNum + 1;
+    }
+
+    private void getQuestionNum_minus()
+    {
+        questionNum = questionNum - 1;
     }
 }
