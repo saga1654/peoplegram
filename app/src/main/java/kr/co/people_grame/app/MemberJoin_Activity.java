4a44208799324e7570a62940b4530f4189b74d30
charles_jo <charles_jo@naver.com>
2015-09-08 13:57:38 +0900

    회원가입 숮

--- app/src/main/java/kr/co/people_grame/app/MemberJoin_Activity.java
+++ app/src/main/java/kr/co/people_grame/app/MemberJoin_Activity.java
@@ -1,4 +1,5 @@
 package kr.co.people_grame.app;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,11 +11,20 @@ import android.widget.EditText;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.util.Log;
+import android.widget.Toast;
 import com.loopj.android.http.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MemberJoin_Activity extends AppCompatActivity {
     private Intent intent;
     private EditText et_usernickname,et_useremail, et_userpw;
+    private Context ActivityContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +33,23 @@ public class MemberJoin_Activity extends AppCompatActivity {
 
     }
 
+    private Boolean emailCheck(CharSequence  email) {
+        String mail = "^[_a-zA-Z0-9-\\.]+@[\\.a-zA-Z0-9-]+\\.[a-zA-Z]+$";
+        Pattern p = Pattern.compile(mail);
+        Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
     public void btn_start(View v) {
         EditText et_usernickname = (EditText) findViewById(R.id.et_activity_memberjoin_nickname);
         EditText et_useremail = (EditText) findViewById(R.id.et_activity_memberjoin_email);
         EditText et_userpw = (EditText) findViewById(R.id.et_activity_memberjoin_pw);
+
+        Boolean email_bool = this.emailCheck(et_useremail.getText().toString());
+        if(email_bool == false) {
+            Toast.makeText(MemberJoin_Activity.this, "이메일 형식이 올바르지 않습니다.다시 확인하시기 바랍니다.", Toast.LENGTH_LONG).show();
+            return;
+        }
 
         if(et_usernickname.getText().toString().equals("")) {
             AlertDialog.Builder alert = new AlertDialog.Builder(MemberJoin_Activity.this);
@@ -47,6 +70,7 @@ public class MemberJoin_Activity extends AppCompatActivity {
                 public void onClick(DialogInterface dialog, int which) {
                 }
             });
+
             alert.setMessage("이메일을 입력해주세요.");
             alert.show();
             return;
@@ -84,15 +108,73 @@ public class MemberJoin_Activity extends AppCompatActivity {
             }
 
             @Override
-            public void onSuccess(String response) {
-                Log.d("people_gram", response);
+            public void onSuccess(String response)
+            {
+                Log.d("people_gram", response.toString());
+                JSONArray json;
+                try {
+                    json = new JSONArray(response);
+                    JSONObject jobj = json.getJSONObject(0);
+                    String code = jobj.getString("code");
+
+
+                    Log.d("people_gram", code);
+                    //Log.d("people_gram", user_data.getString("UID"));
+
+                    switch (code.toString()) {
+                        case "000":
+                            // SharedPreference 넣기
+
+                            JSONArray array = (JSONArray) jobj.get("user_data");
+                            JSONObject user_data = (JSONObject) array.get(0);
+
+
+                            String uid = user_data.get("UID").toString();
+                            String userNickName = user_data.get("USERNICKNAME").toString();
+                            String email = user_data.get("EMAIL").toString();
+
+                            SharedPreferenceUtil.putSharedPreference(ActivityContext, "uid", uid);
+                            SharedPreferenceUtil.putSharedPreference(ActivityContext, "userNickName", userNickName);
+                            SharedPreferenceUtil.putSharedPreference(ActivityContext, "email", email);
+
+
+                            intent = new Intent(MemberJoin_Activity.this, MyQuestion_Activity.class);
+                            startActivity(intent);
+                            finish();
+
+                            Log.d("people_gram", "회원가입 성공");
+
+                            break;
+
+                        case "101":
+                            Log.d("people_gram", "Error");
+                            break;
+                        case "102":
+                            Log.d("people_gram", "Error");
+                            break;
+                        case "998":
+                            Log.d("people_gram", "닉네임이 존재합니다.");
+                            Toast.makeText(MemberJoin_Activity.this, "닉네임이 존재합니다.", Toast.LENGTH_LONG).show();
+                            break;
+                        case "999":
+                            Log.d("people_gram", "동일한 이메일이 존재합니다.");
+                            Toast.makeText(MemberJoin_Activity.this, "동일한 이메일이 존재합니다.", Toast.LENGTH_LONG).show();
+                            break;
+
+                    }
+
+                } catch(JSONException e) {
+                    e.printStackTrace();
+                }
+
+                //Log.d("people_gram", response);
             }
 
 
         });
 
-        intent = new Intent(MemberJoin_Activity.this, MyQuestion_Activity.class);
-        startActivity(intent);
+        //intent = new Intent(MemberJoin_Activity.this, MyQuestion_Activity.class);
+        //startActivity(intent);
 
     }
 
