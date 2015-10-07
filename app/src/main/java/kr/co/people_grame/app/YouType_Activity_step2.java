1bbf1c13cb69a380968b9629a4ea069f9f7c0d9d
charles_jo <charles_jo@naver.com>
2015-10-07 18:48:50 +0900

    upgrade

--- app/src/main/java/kr/co/people_grame/app/YouType_Activity_step2.java
+++ app/src/main/java/kr/co/people_grame/app/YouType_Activity_step2.java
@@ -5,6 +5,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerAdapter;
@@ -16,6 +17,7 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -43,15 +45,17 @@ public class YouType_Activity_step2 extends FragmentActivity {
     private static String people_uid = "";
     private String gubun1 = "";
     private String gubun2 = "";
+    /*
     private Boolean nextCheck = false;
     private Boolean seekCheck = false;
+    */
     private static int dataArray[] = new int[10];
 
     private String userdataArray[];
 
     /* comportment private */
-    private TextView mTxvSeekBarValue, tv_my_question_activity_title;
-    private SeekBar sb_my_question_activity_data;
+    private TextView tv_my_question_activity_title;
+    //private SeekBar sb_my_question_activity_data;
     private Context mcontext;
     private ProgressDialog dialog;
     private Intent intent;
@@ -63,6 +67,11 @@ public class YouType_Activity_step2 extends FragmentActivity {
     private String userData = "";
     private Context ActivityContext;
 
+    private LinearLayout myQuestion_left, myQuestion_right;
+    private Boolean left_onoff = false;
+    private Boolean right_onoff = false;
+
+    private TextView myQuestion_left_textview, myQuestion_right_textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,24 +94,27 @@ public class YouType_Activity_step2 extends FragmentActivity {
         mcontext = this;
 
         /* SeekBar title 가져오기 */
-        mTxvSeekBarValue = (TextView) findViewById(R.id.txvSeekBarValue);
+        /*mTxvSeekBarValue = (TextView) findViewById(R.id.txvSeekBarValue);*/
 
         /* SeekBar value 가져오기 */
-        sb_my_question_activity_data = (SeekBar) findViewById(R.id.sb_my_question_activity_data);
+       // sb_my_question_activity_data = (SeekBar) findViewById(R.id.sb_my_question_activity_data);
         tv_my_question_activity_title = (TextView) findViewById(R.id.tv_my_question_activity_title);
 
+        myQuestion_left = (LinearLayout) findViewById(R.id.myQuestion_left);
+        myQuestion_right = (LinearLayout) findViewById(R.id.myQuestion_right);
 
+        myQuestion_left_textview = (TextView) findViewById(R.id.myQuestion_left_textview);
+        myQuestion_right_textview = (TextView) findViewById(R.id.myQuestion_right_textview);
+
+        getQuestionTitle();
         /* SeekBar Tooltip 좌표 설정 */
+        /*
         int dp = 250;
         int thumbPos = Utilities.getScreenWidth(mcontext) / 2;
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         params.leftMargin = thumbPos - Utilities.getPxToDp(mcontext, dp);
         mTxvSeekBarValue.setLayoutParams(params);
-        /* ########################## */
 
-
-
-        /* seekBar 드래그 좌표 재 설정 */
         sb_my_question_activity_data.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -144,7 +156,7 @@ public class YouType_Activity_step2 extends FragmentActivity {
                 //Log.d("prople_gram", "터치종료="+seekBar.getProgress());
             }
         });
-
+        */
         /* ####### seekBar 드래그 좌표 #####  */
 
 
@@ -164,6 +176,40 @@ public class YouType_Activity_step2 extends FragmentActivity {
             }
         });
         /* ##### navigation 설정 끝 #### */
+    }
+
+    public void myQuestion_default()
+    {
+        left_onoff = false;
+        right_onoff = false;
+        myQuestion_left.setBackgroundColor(Color.rgb(241,241,241));
+        myQuestion_right.setBackgroundColor(Color.rgb(241,241,241));
+    }
+
+    public void myQuestion_left_click(View v) {
+        right_onoff = false;
+        if(left_onoff == false) {
+            left_onoff = true;
+            myQuestion_left.setBackgroundColor(Color.rgb(250,229,4));
+            myQuestion_right.setBackgroundColor(Color.rgb(241,241,241));
+        } else {
+            left_onoff = false;
+            myQuestion_left.setBackgroundColor(Color.rgb(241,241,241));
+            myQuestion_right.setBackgroundColor(Color.rgb(241,241,241));
+        }
+    }
+
+    public void myQuestion_right_click(View v) {
+        left_onoff = false;
+        if(left_onoff == false) {
+            right_onoff = true;
+            myQuestion_left.setBackgroundColor(Color.rgb(241,241,241));
+            myQuestion_right.setBackgroundColor(Color.rgb(250,229,4));
+        } else {
+            right_onoff = false;
+            myQuestion_left.setBackgroundColor(Color.rgb(241, 241, 241));
+            myQuestion_right.setBackgroundColor(Color.rgb(241,241,241));
+        }
     }
 
     private class MyPagerAdapter extends PagerAdapter {
@@ -205,6 +251,7 @@ public class YouType_Activity_step2 extends FragmentActivity {
             //tv_my_question_activity_page.setText(String.valueOf(questionNum) + "/" + "10");
         }
         getQuestionTitle();
+        myQuestion_default();
     }
 
     public void btn_next(View v)
@@ -218,8 +265,6 @@ public class YouType_Activity_step2 extends FragmentActivity {
         if(questionNum == 10) {
             Log.d("people_gram", "마지막페이지");
             //Toast.makeText(this, "마지막페이지", Toast.LENGTH_SHORT).show();
-
-
 
 
             RequestParams params = new RequestParams();
@@ -263,16 +308,17 @@ public class YouType_Activity_step2 extends FragmentActivity {
         } else {
             questionNum_plus();
             pager.setCurrentItem(questionNum);
-            nextCheck = true;
+           /* nextCheck = true;
             sb_my_question_activity_data.setMax(100);
             sb_my_question_activity_data.setProgress(50);
 
             seekCheck = false;
             //sb_my_question_activity_data.setMax(100);
             //sb_my_question_activity_data.setProgress(50);
+            */
         }
         getQuestionTitle();
-
+        myQuestion_default();
 
     }
 
@@ -294,12 +340,26 @@ public class YouType_Activity_step2 extends FragmentActivity {
     /* 데이터 셋팅 */
     private Boolean dataSet()
     {
+        /*
         if(sb_my_question_activity_data.getProgress() == 50) {
             return false;
         } else {
             dataArray[questionNum-1] = sb_my_question_activity_data.getProgress();
             for(int i = 0; i<dataArray.length; i++) {
                 //Log.d("people_gram", i+":::"+String.valueOf(dataArray[i]));
+            }
+            return true;
+        }
+        */
+        if(left_onoff == false && right_onoff == false) {
+            return false;
+        } else {
+            if(left_onoff == true) {
+                dataArray[questionNum-1] = -1;
+            }
+
+            if(right_onoff == true) {
+                dataArray[questionNum-1] = 1;
             }
             return true;
         }
@@ -310,33 +370,63 @@ public class YouType_Activity_step2 extends FragmentActivity {
         switch (questionNum) {
             case 1:
                 tv_my_question_activity_title.setText(getString(R.string.step1));
+                myQuestion_left_textview.setText(getString(R.string.step1_left));
+                myQuestion_right_textview.setText(getString(R.string.step1_right));
+
                 break;
             case 2:
                 tv_my_question_activity_title.setText(getString(R.string.step2));
+
+                myQuestion_left_textview.setText(getString(R.string.step2_left));
+                myQuestion_right_textview.setText(getString(R.string.step2_right));
                 break;
             case 3:
                 tv_my_question_activity_title.setText(getString(R.string.step3));
+
+                myQuestion_left_textview.setText(getString(R.string.step3_left));
+                myQuestion_right_textview.setText(getString(R.string.step3_right));
                 break;
             case 4:
                 tv_my_question_activity_title.setText(getString(R.string.step4));
+
+                myQuestion_left_textview.setText(getString(R.string.step4_left));
+                myQuestion_right_textview.setText(getString(R.string.step4_right));
                 break;
             case 5:
                 tv_my_question_activity_title.setText(getString(R.string.step5));
+
+                myQuestion_left_textview.setText(getString(R.string.step5_left));
+                myQuestion_right_textview.setText(getString(R.string.step5_right));
                 break;
             case 6:
                 tv_my_question_activity_title.setText(getString(R.string.step6));
+
+                myQuestion_left_textview.setText(getString(R.string.step6_left));
+                myQuestion_right_textview.setText(getString(R.string.step6_right));
                 break;
             case 7:
                 tv_my_question_activity_title.setText(getString(R.string.step7));
+
+                myQuestion_left_textview.setText(getString(R.string.step7_left));
+                myQuestion_right_textview.setText(getString(R.string.step7_right));
                 break;
             case 8:
                 tv_my_question_activity_title.setText(getString(R.string.step8));
+
+                myQuestion_left_textview.setText(getString(R.string.step8_left));
+                myQuestion_right_textview.setText(getString(R.string.step8_right));
                 break;
             case 9:
                 tv_my_question_activity_title.setText(getString(R.string.step9));
+
+                myQuestion_left_textview.setText(getString(R.string.step9_left));
+                myQuestion_right_textview.setText(getString(R.string.step9_right));
                 break;
             case 10:
                 tv_my_question_activity_title.setText(getString(R.string.step10));
+
+                myQuestion_left_textview.setText(getString(R.string.step10_left));
+                myQuestion_right_textview.setText(getString(R.string.step10_right));
                 break;
         }
 
