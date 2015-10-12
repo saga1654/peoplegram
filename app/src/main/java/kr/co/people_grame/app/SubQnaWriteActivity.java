145326c598d109f9e077de567e783a8795632c4f
saga1654 <saga1654@naver.com>
2015-10-12 14:42:21 +0900

    Ver 0.11

--- app/src/main/java/kr/co/people_grame/app/SubQnaWriteActivity.java
+++ app/src/main/java/kr/co/people_grame/app/SubQnaWriteActivity.java
@@ -2,6 +2,7 @@ package kr.co.people_grame.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,24 +15,23 @@ public class SubQnaWriteActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_sub_qna_write, menu);
-        return true;
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                finish();
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        //if (id == R.id.action_settings) {
-        //    return true;
-        //}
-
-        return super.onOptionsItemSelected(item);
+    public void finish()
+    {
+        super.finish();
+        overridePendingTransition(R.anim.end_enter, R.anim.end_exit);
     }
 }
