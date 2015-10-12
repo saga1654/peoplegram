863c1a5c55588592a19faa0a3c810fcc4c50b75a
saga1654 <saga1654@naver.com>
2015-10-12 20:30:18 +0900

    Ver 0.12

--- app/src/main/java/kr/co/people_grame/app/LogoActivity.java
+++ app/src/main/java/kr/co/people_grame/app/LogoActivity.java
@@ -3,8 +3,12 @@ package kr.co.people_grame.app;
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Matrix;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -25,6 +29,9 @@ import android.view.animation.TranslateAnimation;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
@@ -40,9 +47,120 @@ public class LogoActivity extends AppCompatActivity {
     private LinearLayout login_activity_li_logo, login_activity_li_btn;
     private ProgressDialog dialog;
 
+    private String token = "";
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+
+    /**
+     * Instance ID를 이용하여 디바이스 토큰을 가져오는 RegistrationIntentService를 실행한다.
+     */
+    public void getInstanceIdToken() {
+        if (checkPlayServices()) {
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
+    }
+
+    /**
+     * Google Play Service를 사용할 수 있는 환경이지를 체크한다.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i("mainActivity", "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 앱이 실행되어 화면에 나타날때 LocalBoardcastManager에 액션을 정의하여 등록한다.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_READY));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_GENERATING));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+
+    }
+
+    /**
+     * 앱이 화면에서 사라지면 등록된 LocalBoardcast를 모두 삭제한다.
+     */
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
+    }
+
+
+
+    /**
+     * LocalBroadcast 리시버를 정의한다. 토큰을 획득하기 위한 READY, GENERATING, COMPLETE 액션에 따라 UI에 변화를 준다.
+     */
+    public void registBroadcastReceiver(){
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+
+
+                if(action.equals(QuickstartPreferences.REGISTRATION_READY)){
+                    // 액션이 READY일 경우
+                    //mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
+                    //mInformationTextView.setVisibility(View.GONE);
+                    Log.d("people_gram", "READY");
+                } else if(action.equals(QuickstartPreferences.REGISTRATION_GENERATING)){
+                    // 액션이 GENERATING일 경우
+                    //mRegistrationProgressBar.setVisibility(ProgressBar.VISIBLE);
+                    //mInformationTextView.setVisibility(View.VISIBLE);
+                    //mInformationTextView.setText(getString(R.string.registering_message_generating));
+
+                    Log.d("people_gram", "GENERATING");
+
+                } else if(action.equals(QuickstartPreferences.REGISTRATION_COMPLETE)){
+                    // 액션이 COMPLETE일 경우
+                    //mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
+                    //mRegistrationButton.setText(getString(R.string.registering_message_complete));
+                    //mRegistrationButton.setEnabled(false);
+                    String token = intent.getStringExtra("token");
+                    SharedPreferenceUtil.putSharedPreference(LogoActivity.this, "token", token);
+                    //Log.d("people_gram", "토큰="+token);
+                    //mInformationTextView.setText(token);
+                }
+
+            }
+        };
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(SharedPreferenceUtil.getSharedPreference(LogoActivity.this, "token") == "") {
+            registBroadcastReceiver();
+            getInstanceIdToken();
+        } else {
+
+
+        }
+
+        Log.d("people_gram", "토큰저장="+SharedPreferenceUtil.getSharedPreference(LogoActivity.this, "token"));
+
+        //getInstanceIdToken();
+
+
 
 
         login_activity_li_logo = (LinearLayout) findViewById(R.id.login_activity_li_logo);
@@ -116,6 +234,7 @@ public class LogoActivity extends AppCompatActivity {
                                 SharedPreferenceUtil.putSharedPreference(LogoActivity.this, "my_speed", jobj.getString("MY_SPEED"));
                                 SharedPreferenceUtil.putSharedPreference(LogoActivity.this, "my_control", jobj.getString("MY_CONTROL"));
                                 SharedPreferenceUtil.putSharedPreference(LogoActivity.this, "email", jobj.getString("EMAIL"));
+
 
                                 Intent intent = new Intent(LogoActivity.this, MainActivity.class);
                                 startActivity(intent);
