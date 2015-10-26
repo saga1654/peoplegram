a0a9ecc1cba5b38db7783be458ce9c4ef61fa3f4
saga1654 <saga1654@naver.com>
2015-10-26 22:27:04 +0900

    Ver 0.22

--- app/src/main/assets/js/name.js
+++ app/src/main/assets/js/name.js
@@ -8,3 +8,74 @@
 		$(".Name2").text(inputTxt2);
 
 	}
+
+
+	function htmlWidth()
+	{
+		var width = Math.floor(document.body.clientWidth * 0.85);
+		return width;
+	}
+
+	function chartLine1(x, y) {
+		var width = htmlWidth();
+		var height = width;
+
+		var x_my = 0;
+		var y_my = 0;
+
+		if(x < 0){
+        	x_my = Math.floor((parseInt(width) / 11) * (5 + parseInt(x)));
+        }
+
+        if(x > 0){
+        	x_my = Math.floor((parseInt(width) / 11) * (6 + parseInt(x)));
+        }
+
+        if(y > 0){
+        	y_my = Math.floor((parseInt(height) / 11) * (5 - parseInt(y)));
+        }
+
+        if(y < 0){
+        	y_my = Math.floor((parseInt(height) / 11) * (6 - parseInt(y)));
+        }
+
+        $("#chart_line1").css({
+        	"top" : y_my+"px",
+        	"left" : x_my+"px"
+        });
+        //$("#chart_line1").html("내용");
+
+        //document.write(width + ":::" + x + ":::" + y + ":::" + x_my + ":::" + y_my);
+	}
+
+	function chartLine2(x, y) {
+    	var width = htmlWidth();
+        var height = width;
+
+		var x_my = 0;
+		var y_my = 0;
+
+		if(x < 0){
+        	x_my = Math.floor((parseInt(width) / 11) * (5 + parseInt(x)));
+        }
+
+        if(x > 0){
+        	x_my = Math.floor((parseInt(width) / 11) * (6 + parseInt(x)));
+        }
+
+        if(y > 0){
+        	y_my = Math.floor((parseInt(height) / 11) * (5 - parseInt(y)));
+        }
+
+        if(y < 0){
+        	y_my = Math.floor((parseInt(height) / 11) * (6 - parseInt(y)));
+        }
+
+        $("#chart_line2").css({
+            "top" : y_my+"px",
+        	"left" : x_my+"px"
+        });
+        //document.write(width + ":::" + x + ":::" + y + ":::" + x_my + ":::" + y_my);
+
+        //$("#chart_line2").html(x_my + ":::" + y_my);
+    }
--- app/src/main/java/kr/co/people_grame/app/GramPopupMyTypeActivity.java
+++ app/src/main/java/kr/co/people_grame/app/GramPopupMyTypeActivity.java
@@ -12,6 +12,9 @@ import android.widget.Toast;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class GramPopupMyTypeActivity extends AppCompatActivity {
 
 
@@ -53,6 +56,43 @@ public class GramPopupMyTypeActivity extends AppCompatActivity {
 
             @Override
             public void onSuccess(String response) {
+
+                JSONObject jobj = null;
+                try {
+                    jobj = new JSONObject(response);
+
+                    if(jobj.getString("code").equals("999")) {
+                        intent.putExtra("code", "999");
+                        Toast.makeText(GramPopupMyTypeActivity.this, "평가한 인원수가 부족합니다\n피플들에게 요청해주세요.", Toast.LENGTH_LONG).show();
+                        finish();
+                    } else {
+                        String my_type = SharedPreferenceUtil.getSharedPreference(GramPopupMyTypeActivity.this, "mytype");
+                        String people_type = jobj.getString("people_type");
+                        String my_speed = jobj.getString("my_speed");
+                        String my_control = jobj.getString("my_control");
+                        String people_speed = jobj.getString("people_speed");
+                        String people_control = jobj.getString("people_control");
+                        String gubun1 = jobj.getString("gubun1");
+
+                        intent.putExtra("code", "000");
+                        intent.putExtra("mytype", "A");
+                        intent.putExtra("people_type", "A");
+                        intent.putExtra("gubun1", gubun1);
+                        intent.putExtra("my_speed", my_speed);
+                        intent.putExtra("my_control", my_control);
+                        intent.putExtra("people_speed", people_speed);
+                        intent.putExtra("people_control", people_control);
+                        startActivity(intent);
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+
+                /*
                 if(response.equals("999") == false) {
                     intent.putExtra("code", "000");
                     intent.putExtra("people_type", response);
@@ -64,6 +104,7 @@ public class GramPopupMyTypeActivity extends AppCompatActivity {
                     Toast.makeText(GramPopupMyTypeActivity.this, "평가한 인원수가 부족합니다\n피플들에게 요청해주세요.", Toast.LENGTH_LONG).show();
                     finish();
                 }
+                */
             }
         });
     }
