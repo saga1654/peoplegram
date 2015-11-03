665ebc6a2475a0387ad0dc7ff7031da91b8bcee2
charles_jo <charles_jo@naver.com>
2015-11-03 16:58:34 +0900

    update151103(2)

--- app/src/main/assets/js/name.js
+++ app/src/main/assets/js/name.js
@@ -83,14 +83,18 @@
     function type_chart(my_speed, my_control, people_speed, people_control)
     	{
     		var my_total = Math.abs(my_speed * my_control);
-    		var my_per_up = parseInt((my_total * 13) / 5);
-    		var my_per_down = 65 - my_per_up;
+    		var my_per = parseInt((my_total * 4));
     		var my_type = "";
 
+			$("#type1_title").css({"color":"#fff","background-color":"#32354d","font-size":"0.85em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});	
+			$("#type1_per").html(my_per+"%");			
 
+			
     		if(my_speed > 0 && my_control > 0) {
-    			$("#type1_bg_color").css({"background-color" : "#ff8a55"});
-    			$("#type1_text").css({"color":"#ff8a55"});
+    			//$("#type1_bg_color").css({"background-color" : "#ff8a55"});
+    			
+				$("#type1_per").css({"color":"#ff8a55","background-color":"#4d4d4d","font-size":"2.0em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
+				$("#type1_text").css({"color":"#ff8a55","background-color":"#4d4d4d","font-size":"0.85em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
 
     			if(my_speed <= 5 && my_control <= 1){
     			   	if(my_speed == 1 && my_control == 1){
@@ -105,8 +109,11 @@
     			}
     		}
     		if(my_speed > 0 && my_control < 0) {
-    			$("#type1_bg_color").css({"background-color" : "#37afec"});
-    			$("#type1_text").css({"color":"#37afec"});
+    			//$("#type1_bg_color").css({"background-color" : "#37afec"});
+    			
+				$("#type1_per").css({"color":"#37afec","background-color":"#4d4d4d","font-size":"2.0em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
+				$("#type1_text").css({"color":"#37afec","background-color":"#4d4d4d","font-size":"0.85em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
+
 
     			if(my_speed <= 5 && my_control >= -1){
     				if(my_speed == 1 && my_control == -1){
@@ -121,8 +128,10 @@
 				}
     		}
     		if(my_speed < 0 && my_control > 0) {
-    			$("#type1_bg_color").css({"background-color" : "#52d935"});
-    			$("#type1_text").css({"color":"#52d935"});
+    			//$("#type1_bg_color").css({"background-color" : "#52d935"});
+    			
+				$("#type1_per").css({"color":"#52d935","background-color":"#4d4d4d","font-size":"2.0em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
+				$("#type1_text").css({"color":"#52d935","background-color":"#4d4d4d","font-size":"0.85em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
 
     			if(my_speed >= -5 && my_control <= 1){
     				if(my_speed == -1 && my_control == 1){
@@ -137,8 +146,10 @@
 				}
     		}
     		if(my_speed < 0 && my_control < 0) {
-    			$("#type1_bg_color").css({"background-color" : "#aa64f8"});
-    			$("#type1_text").css({"color":"#aa64f8"});
+    			//$("#type1_bg_color").css({"background-color" : "#aa64f8"});
+    			
+				$("#type1_per").css({"color":"#aa64f8","background-color":"#4d4d4d","font-size":"2.0em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
+				$("#type1_text").css({"color":"#aa64f8","background-color":"#4d4d4d","font-size":"0.85em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
 
     			if(my_speed >= -5 && people_control >= -1){
 					if(my_speed == -1 && my_control == -1){
@@ -153,26 +164,38 @@
 				}
     		}
 
+			
+			var people_total = Math.abs(people_speed * people_control);
+    		var people_per = parseInt((people_total * 4));
 
+			$("#type2_title").css({"color":"#fff","background-color":"#32354d","font-size":"0.85em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
+			$("#type2_per").html(people_per+"%");
+				
+    		
     		if(people_speed > 0 && people_control > 0) {
-    			$("#type2_bg_color").css({"background-color" : "#ff8a55"});
+    			//$("#type2_bg_color").css({"background-color" : "#ff8a55"});
     			$("#type2_text").css({"color":"#ff8a55"});
+
+				$("#type2_per").css({"color":"#ff8a55","background-color":"#4d4d4d","font-size":"2.0em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
+				$("#type2_text").css({"color":"#ff8a55","background-color":"#4d4d4d","font-size":"0.85em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
 
     			if(my_speed <= 5 && my_control <= 1){
 					if(my_speed == 1 && my_control == 1){
-						$("#type1_text").html("분석형,우호형 특징을 가진 주도형");
+						$("#type2_text").html("분석형,우호형 특징을 가진 주도형");
 					}else{
-						$("#type1_text").html("분석형 특징을 가진 주도형");
+						$("#type2_text").html("분석형 특징을 가진 주도형");
 					}
 				}else if(my_speed <= 1 && my_control <= 5){
-					$("#type1_text").html("우호형 특성을 가진 주도형");
+					$("#type2_text").html("우호형 특성을 가진 주도형");
 				}else{
-					$("#type1_text").html("확실한 주도형");
+					$("#type2_text").html("확실한 주도형");
 				}
     		}
     		if(people_speed > 0 && people_control < 0) {
-    			$("#type2_bg_color").css({"background-color" : "#37afec"});
-    			$("#type2_text").css({"color":"#37afec"});
+    			//$("#type2_bg_color").css({"background-color" : "#37afec"});
+    			
+				$("#type2_per").css({"color":"#37afec","background-color":"#4d4d4d","font-size":"2.0em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
+				$("#type2_text").css({"color":"#37afec","background-color":"#4d4d4d","font-size":"0.85em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
 
     			if(people_speed <= 5 && people_control >= -1){
 					if(people_speed == 1 && people_control == -1){
@@ -187,8 +210,10 @@
 				}
     		}
     		if(people_speed < 0 && people_control > 0) {
-    			$("#type2_bg_color").css({"background-color" : "#52d935"});
-    			$("#type2_text").css({"color":"#52d935"});
+    			//$("#type2_bg_color").css({"background-color" : "#52d935"});
+    		
+				$("#type2_per").css({"color":"#52d935","background-color":"#4d4d4d","font-size":"2.0em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
+				$("#type2_text").css({"color":"#52d935","background-color":"#4d4d4d","font-size":"0.85em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
 
     			if(people_speed >= -5 && people_control <= 1){
 					if(people_speed == -1 && people_control == 1){
@@ -203,9 +228,10 @@
 				}
     		}
     		if(people_speed < 0 && people_control < 0) {
-    			$("#type2_bg_color").css({"background-color" : "#aa64f8"});
-    			$("#type2_text").css({"color":"#aa64f8"});
-
+    			//$("#type2_bg_color").css({"background-color" : "#aa64f8"});
+    			
+				$("#type2_per").css({"color":"#aa64f8","background-color":"#4d4d4d","font-size":"2.0em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
+				$("#type2_text").css({"color":"#aa64f8","background-color":"#4d4d4d","font-size":"0.85em","padding-top":"1.0em","padding-bottom":"1.0em","text-align":"center"});
     			if(people_speed >= -5 && people_control >= -1){
 					if(people_speed == -1 && people_control == -1){
 						$("#type2_text").html("우호형,분석형 특징을 가진 표출형");
@@ -221,18 +247,17 @@
 
 
 
-    		var people_total = Math.abs(people_speed * people_control);
-    		var people_per_up = parseInt((people_total * 13) / 5);
-    		var people_per_down = 65 - people_per_up;
+    		
 
     		console.log(my_per_up + ":::" + my_per_down + ":::" + people_per_up + ":::" + people_per_down);
 
 
-
+			/*
     		$("#type1_text").css({"width" : my_per_down+"%"});
     		$("#type1_bg").css({"width" : my_per_up+"%"});
 
     		$("#type2_text").css({"width" : people_per_down+"%"});
     		$("#type2_bg").css({"width" : "20%"});
+			*/
 
     	}
