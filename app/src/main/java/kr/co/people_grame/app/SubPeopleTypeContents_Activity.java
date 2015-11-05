529652b76aac20e39f843641eda6a9813d3d350a
charles_jo <charles_jo@naver.com>
2015-11-05 16:09:02 +0900

    update151104

--- app/src/main/java/kr/co/people_grame/app/SubPeopleTypeContents_Activity.java
+++ app/src/main/java/kr/co/people_grame/app/SubPeopleTypeContents_Activity.java
@@ -117,8 +117,8 @@ public class SubPeopleTypeContents_Activity extends AppCompatActivity {
 
 
 
-        //people_content_webview.loadUrl("file:///android_asset/peopletype_my/" + gubun1 + "/" + gubun1 + "_MY_" + mytype + peopletype + ".html");
-        people_content_webview.loadUrl("file:///android_asset/peopletype_my/" + gubun1 + "/" + gubun1 + "_MY_AA.html");
+        people_content_webview.loadUrl("file:///android_asset/peopletype_my/" + gubun1 + "/" + gubun1 + "_MY_" + mytype + peopletype + ".html");
+        //people_content_webview.loadUrl("file:///android_asset/peopletype_my/" + gubun1 + "/" + gubun1 + "_MY_AA.html");
     }
 
     @Override
