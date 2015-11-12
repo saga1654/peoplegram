ad385b36829c1155c9a3f633ba8901415f251bbc
charles_jo <charles_jo@naver.com>
2015-11-12 21:13:13 +0900

    update151112

--- app/src/main/java/kr/co/people_grame/app/ProfileActivity.java
+++ app/src/main/java/kr/co/people_grame/app/ProfileActivity.java
@@ -251,7 +251,9 @@ public class ProfileActivity extends AppCompatActivity {
     public void btn_back(View v) {
         finish();
     }
-
+    public void closeBtn(View v) {
+        finish();
+    }
     public void finish()
     {
         super.finish();
