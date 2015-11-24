7f7fcf70a562673bab47d43c9e3103c57c180b71
saga1654 <saga1654@naver.com>
2015-11-24 10:07:29 +0900

    Ver 0.97 - 유형특징 UI 개선

--- app/src/main/java/kr/co/people_gram/app/SubPeopleFragment.java
+++ app/src/main/java/kr/co/people_gram/app/SubPeopleFragment.java
@@ -83,6 +83,7 @@ public class SubPeopleFragment extends Fragment {
     private Switch listview_youtype_switch;
 
     private Boolean people_popupview = false;
+    private String my_sub_type = "";
 
     private NetworkCheck nc;
 
@@ -211,24 +212,28 @@ public class SubPeopleFragment extends Fragment {
                                 if(data.getString("code").equals("000")) {
                                     switch (data.getString("peopleType")) {
                                         case "A":
+                                            my_sub_type = "A";
                                             listview_proplelist_img.setImageResource(R.mipmap.peoplelist_type_a);
                                             people_detail_view.setVisibility(View.VISIBLE);
                                             people_detail_view_title.setVisibility(View.VISIBLE);
                                             break;
 
                                         case "I":
+                                            my_sub_type = "I";
                                             listview_proplelist_img.setImageResource(R.mipmap.peoplelist_type_i);
                                             people_detail_view.setVisibility(View.VISIBLE);
                                             people_detail_view_title.setVisibility(View.VISIBLE);
                                             break;
 
                                         case "D":
+                                            my_sub_type = "D";
                                             listview_proplelist_img.setImageResource(R.mipmap.peoplelist_type_d);
                                             people_detail_view.setVisibility(View.VISIBLE);
                                             people_detail_view_title.setVisibility(View.VISIBLE);
                                             break;
 
                                         case "E":
+                                            my_sub_type = "E";
                                             listview_proplelist_img.setImageResource(R.mipmap.peoplelist_type_e);
                                             people_detail_view.setVisibility(View.VISIBLE);
                                             people_detail_view_title.setVisibility(View.VISIBLE);
@@ -303,8 +308,14 @@ public class SubPeopleFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), SubMyType_Activity.class);
-                intent.putExtra("mytype", SharedPreferenceUtil.getSharedPreference(getActivity(), "mytype"));
+                if(listview_youtype_switch.isChecked() == false) {
+                    intent.putExtra("mytype", SharedPreferenceUtil.getSharedPreference(getActivity(), "mytype"));
+
+                } else {
+                    intent.putExtra("mytype", my_sub_type);
+                }
                 startActivity(intent);
+
                 //getActivity().overridePendingTransition(R.anim.slide_up_info, R.anim.slide_down_info);
             }
         });
