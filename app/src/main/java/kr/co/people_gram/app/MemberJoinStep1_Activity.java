b7bdade2da38b81d58a7b8597234077092de6ead
saga1654 <saga1654@naver.com>
2015-12-01 16:45:37 +0900

    Ver 0.99

--- app/src/main/java/kr/co/people_gram/app/MemberJoinStep1_Activity.java
+++ app/src/main/java/kr/co/people_gram/app/MemberJoinStep1_Activity.java
@@ -1,6 +1,7 @@
 package kr.co.people_gram.app;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,11 +13,18 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MemberJoinStep1_Activity extends AppCompatActivity {
     private String PhoneNumber, telecom;
     private MemberData md;
     private LinearLayout agree_btn, userinfo_btn;
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,42 @@ public class MemberJoinStep1_Activity extends AppCompatActivity {
             md.set_phone(PhoneNumber);
             md.set_telecom(telecom);
             Log.d("people_gram", PhoneNumber);
+
+
+            RequestParams params = new RequestParams();
+            params.put("PhoneNumber", PhoneNumber);
+
+            HttpClient.post("/user/telephoneCheck", params, new AsyncHttpResponseHandler() {
+                public void onStart() {
+                    dialog = ProgressDialog.show(MemberJoinStep1_Activity.this, "", "데이터 수신중");
+                }
+
+                public void onFinish() {
+                    dialog.dismiss();
+                }
+
+                @Override
+                public void onSuccess(String response) {
+                    Log.d("people_gram", response);
+
+                    if(response.equals("999")) {
+                        AlertDialog.Builder alert = new AlertDialog.Builder(MemberJoinStep1_Activity.this);
+                        alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+                        alert.setMessage("가입된 내역이 존재합니다.다시 확인 부탁드립니다.");
+                        alert.show();
+                    }
+
+                    //Log.d("people_gram", response);
+                }
+
+
+            });
+
         } catch (Exception e) {
 
             AlertDialog.Builder alert = new AlertDialog.Builder(MemberJoinStep1_Activity.this);
