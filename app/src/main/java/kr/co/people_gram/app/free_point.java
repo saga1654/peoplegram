fc8225fa02804c96c2ec2def5b4b7c749a26e2cb
charles_jo <charles_jo@naver.com>
2015-12-01 13:43:49 +0900

    UPDATE1130

---
+++ app/src/main/java/kr/co/people_gram/app/free_point.java
@@ -0,0 +1,47 @@
+package kr.co.people_gram.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+
+public class free_point extends AppCompatActivity {
+
+    private WebView free;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_free_point);
+
+        free = (WebView) findViewById(R.id.free);
+        free.loadUrl("http://121.162.209.41:81/User/free");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                finish();
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void free_prevBtn(View v) {
+        finish();
+    }
+
+    public void finish()
+    {
+        super.finish();
+        overridePendingTransition(R.anim.end_enter, R.anim.end_exit);
+    }
+}
