809436fddeab855b3ea852b0ad7687e3f72bc2df
saga1654 <saga1654@naver.com>
2015-12-09 22:04:04 +0900

    Ver 1.1.2

--- app/src/main/java/kr/co/people_gram/app/PeopleSync_Activity.java
+++ app/src/main/java/kr/co/people_gram/app/PeopleSync_Activity.java
@@ -9,14 +9,20 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import android.os.Handler;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class PeopleSync_Activity extends AppCompatActivity {
@@ -24,16 +30,22 @@ public class PeopleSync_Activity extends AppCompatActivity {
     private String userString = "";
     private ProgressBar dialog;
     private TextView progressBarTxt;
-    private ProgressDialog dialog2;
+    //private ProgressDialog dialog2;
     private int totalCnt = 0;
     private int nowCnt = 0;
 
+    int min, sec;
+
     private boolean check = false;
+    private ImageView imageView1;
+    static int per = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_people_sync_);
+
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
 
         try {
             Intent intent = getIntent();
@@ -49,7 +61,7 @@ public class PeopleSync_Activity extends AppCompatActivity {
             check = false;
         }
 
-        dialog2 = ProgressDialog.show(PeopleSync_Activity.this, "", "데이터 수신중");
+        //dialog2 = ProgressDialog.show(PeopleSync_Activity.this, "", "데이터 수신중");
 
         dialog = (ProgressBar) findViewById(R.id.progressBar);
         progressBarTxt = (TextView) findViewById(R.id.progressBarTxt);
@@ -76,14 +88,24 @@ public class PeopleSync_Activity extends AppCompatActivity {
         public void handleMessage(android.os.Message msg)
         {
 
+
+            if(per >= 0 && per < 75) {
+                imageView1.setImageResource(R.drawable.start_3);
+            } else {
+                imageView1.setImageResource(R.drawable.start_4);
+            }
+
+
+
             dialog.setVisibility(View.VISIBLE);
             progressBarTxt.setVisibility(View.VISIBLE);
             dialog.setMax(totalCnt);
             dialog.incrementProgressBy(1);
+
             if(nowCnt == totalCnt) {
-                progressBarTxt.setText("데이터 등록중입니다.");
+                progressBarTxt.setText("기다려주세요.");
             } else {
-                progressBarTxt.setText(nowCnt + "/" + totalCnt);
+                progressBarTxt.setText("설치중");
             }
             //nowCnt
             //dialog.setProgress(Integer.parseInt(String.valueOf(msg)));
@@ -94,13 +116,16 @@ public class PeopleSync_Activity extends AppCompatActivity {
 
 
     private void readContacts() {
-
         Thread background = new Thread(new Runnable() {
             @Override
             public void run() {
                 //dialog.setVisibility(View.VISIBLE);
                 //dialog.setMax(cur.getCount());
+
                 try {
+
+                    //SimpleDateFormat sdf = new SimpleDateFormat("ss");
+
 
 
 
@@ -109,6 +134,7 @@ public class PeopleSync_Activity extends AppCompatActivity {
                     Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
                     userdataArray = new String[cur.getCount()];
                     String phone = null;
+
                     //Log.d("people_gram", "count=" + String.valueOf(cur.getCount()));
 
                     totalCnt = cur.getCount();
@@ -117,6 +143,7 @@ public class PeopleSync_Activity extends AppCompatActivity {
                         int i = 0;
                         int total = 1;
                         while (cur.moveToNext()) {
+
 
                             //Log.d("people_gram", total + ":::" + totalCnt);
                             String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
@@ -136,6 +163,11 @@ public class PeopleSync_Activity extends AppCompatActivity {
 
                                     //Log.d("people_gram", name+":::"+phone);
 
+
+
+                                    per = nowCnt  * 100;
+                                    per = per / totalCnt;
+
                                     nowCnt = total;
                                 }
                                 pCur.close();
@@ -144,7 +176,7 @@ public class PeopleSync_Activity extends AppCompatActivity {
                             handler.sendEmptyMessage(total);
                             if(total == totalCnt) {
 
-                                SystemClock.sleep(1000L);
+
 
                                 Log.d("people_gram", "종료");
 
@@ -158,7 +190,7 @@ public class PeopleSync_Activity extends AppCompatActivity {
                                     }
 
                                     public void onFinish() {
-                                        dialog2.dismiss();
+                                        //dialog2.dismiss();
                                     }
 
                                     @Override
@@ -176,6 +208,8 @@ public class PeopleSync_Activity extends AppCompatActivity {
                                                 overridePendingTransition(R.anim.slide_close_down_info, R.anim.slide_clode_up_info);
                                                 finish();
                                             }
+
+                                            SystemClock.sleep(1000L);
                                         }
                                         /*
                                         Log.d("people_gram", response);
@@ -205,5 +239,16 @@ public class PeopleSync_Activity extends AppCompatActivity {
     {
         super.onStop();
         isRunning = false;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                return true;
+
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
