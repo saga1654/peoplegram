1d750cce31fb5ae136562411d2a83d643cdf7c9f
charles_jo <charles_jo@naver.com>
2016-02-17 16:16:35 +0900

    update 20160217

--- app/src/main/java/kr/co/people_gram/app/KakaoMsgActivity.java
+++ app/src/main/java/kr/co/people_gram/app/KakaoMsgActivity.java
@@ -4,20 +4,15 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 
-import com.kakao.kakaolink.KakaoLink;
-
 public class KakaoMsgActivity extends AppCompatActivity {
 
     private EditText et_contents;
-    private String msg = "이 테스트 해봤어요? \n나는 누구일까요? \n다른 사람은 나를 어떻게 생각할까요? \n좋은 관계를 만들려면 어떻게 해야 할까요? \n\n나와 다른 사람의 행동 유형이 궁금하다면 지금 진단해 보세요! \n(아이폰유저는 1월말 출시예정)";
+    private String msg = "이 테스트 해봤어요? \n나는 누구일까요? \n다른 사람은 나를 어떻게 생각할까요? \n좋은 관계를 만들려면 어떻게 해야 할까요? \n\n나와 다른 사람의 행동 유형이 궁금하다면 지금 진단해 보세요! \n";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
